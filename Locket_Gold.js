ae38d61acff3d0759f2fcac5c8a701d08d8f0ed8
HuuTriet <162295740+HuuTriet@users.noreply.github.com>
2026-04-25 15:34:23 +0700

    Update Locket_Gold.js

--- Locket_Gold.js
+++ Locket_Gold.js
@@ -4,7 +4,7 @@ var obj = JSON.parse(body);
 if (obj.subscriber) {
     obj.subscriber.entitlements = {
         "gold": {
-            "expires_date": "2099-12-31T23:59:59Z",
+            "expires_date": "2030-12-31T23:59:59Z",
             "product_identifier": "locket_gold_yearly",
             "purchase_date": "2023-01-01T00:00:00Z"
         }
@@ -12,7 +12,7 @@ if (obj.subscriber) {
     obj.subscriber.subscriptions = {
         "locket_gold_yearly": {
             "billing_issues_detected_at": null,
-            "expires_date": "2099-12-31T23:59:59Z",
+            "expires_date": "2030-12-31T23:59:59Z",
             "is_sandbox": false,
             "original_purchase_date": "2023-01-01T00:00:00Z",
             "ownership_type": "PURCHASED",

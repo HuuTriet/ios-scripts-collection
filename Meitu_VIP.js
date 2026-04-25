9aebb1472cd5db73d4c4c1aebc787130b09990fa
HuuTriet <162295740+HuuTriet@users.noreply.github.com>
2026-04-25 15:31:54 +0700

    Modify Meitu_VIP.js to grant VIP status

---
+++ Meitu_VIP.js
@@ -0,0 +1,11 @@
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+if (obj.data) {
+    obj.data.is_vip = true;
+    obj.data.vip_type = 1;
+    obj.data.expire_time = "2099-12-31 23:59:59";
+}
+
+$done({body: JSON.stringify(obj)});
